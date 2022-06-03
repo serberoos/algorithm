04cf4dc2ce39a827ee16efef0eaf62712ac3914a
JaeGeun.Song <jae99c@gmail.com>
2022-06-03 22:54:29 +0900

    add reetcode problem 217

---
+++ leetcode/217 Contains Duplicate.py
@@ -0,0 +1,13 @@
+class Solution {
+	func containsDuplicate(_ nums: [Int]) -> Bool {
+		var numSet = Set<Int>()
+
+		for num in nums{
+			if numSet.contains(num){
+				return true
+			}
+			numSet.insert(num)
+		}
+		return false
+	}
+}
